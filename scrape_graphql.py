5092889cd94b6159b37896ffaa7193de89aa98c7
Adam Taylor <afranklint16@gmail.com>
2024-06-11 10:48:10 -0600

    Parse m3u8 all the way and put all links in a sqlite db

--- scrape_graphql.py
+++ scrape_graphql.py
@@ -1,6 +1,58 @@
 #!/usr/bin/env python3
-import requests # pip3 install requests
+import requests
+import sqlite3
+import m3u8
 import re
+
+# Connect to the SQLite database
+conn = sqlite3.connect('chosen_links.db')
+c = conn.cursor()
+
+# Create table
+c.execute('''
+    CREATE TABLE IF NOT EXISTS links (
+        url text PRIMARY KEY,
+        season integer NOT NULL,
+        episode integer NOT NULL,
+        title text NOT NULL,
+        type text NOT NULL,
+        language text,
+        resolution text,
+        bandwidth integer,
+        average_bandwidth integer
+    )
+''')
+
+def parse_m3u8(url, season, episode, title):
+    m3u8_obj = m3u8.load(url)
+
+    # Parse media URLs
+    for media in m3u8_obj.media:
+        c.execute("INSERT INTO links VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", (
+            media.uri,
+            season,
+            episode,
+            title,
+            media.type.lower(),
+            media.language,
+            None,
+            None,
+            None
+        ))
+
+    # Parse playlists
+    for playlist in m3u8_obj.playlists:
+        c.execute("INSERT INTO links VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", (
+            playlist.uri,
+            season,
+            episode,
+            title,
+            'video',
+            None,
+            str(playlist.stream_info.resolution[1]) + 'p',
+            playlist.stream_info.bandwidth,
+            playlist.stream_info.average_bandwidth
+        ))
 
 query = """
 query Videos($ChannelID: ID!, $OrderByField: VideoOrderField!, $OrderByDirection: OrderDirection!) {
@@ -39,12 +91,21 @@ videos = response.json()['data']['videos']['edges']
 
 def filter_video_for_full_episodes(video):
     tags = video['node']['tags']['edges']
-    # check if tags include a node with value 'Full Episode'
-    return any(map(lambda tag: tag['node']['value'] == 'Full Episode', tags)) # and re.search(r'^S[0-9]+ E[0-9]+: ', video['node']['title'])
+    return any(map(lambda tag: tag['node']['value'] == 'Full Episode', tags))
+
 full_episodes = sorted(filter(filter_video_for_full_episodes, videos), key=lambda video: video['node']['title'])
 
-# use the url from the video node if it exists, otherwise compute it by interpolating the video id into the appropriate url pattern
-hls_urls = [video['node']['url'] or f'https://api.frontrow.cc/channels/12884901895/VIDEO/{video['node']['id']}/hls.m3u8' for video in full_episodes]
-
 for i, video in enumerate(full_episodes):
-    print(hls_urls[i], video['node']['title'], video['node']['description'], sep='\t')
+    url = video['node']['url'] or f"https://api.frontrow.cc/channels/12884901895/VIDEO/{video['node']['id']}/hls.m3u8"
+    match = re.search(r'S([0-9]+) E([0-9]+)', video['node']['title'])
+    if match is not None:
+        season, episode = map(int, match.groups())
+        title = re.sub(r'S([0-9]+) E([0-9]+): ', '', video['node']['title'])
+    else:
+        season, episode = 0, 0
+        title = video['node']['title']
+    parse_m3u8(url, season, episode, title)
+
+# Commit the changes and close the connection
+conn.commit()
+conn.close()
